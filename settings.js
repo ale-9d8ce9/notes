37f65e03a380784bee65028443340f55ad327b67
ale9d8ce9 <ale9d8ce9@gmail.com>
2025-11-10 18:45:18 +0100

    changed theming + blurred balls in note background

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ settings = {
     maxZoom: 20,
     gestureSensitivity: {
         moveWithCtrl: 1,
-        zoom: 0.01,
-        moveWithFinger: 2
+        zoom: 0.015,
+        moveWithFinger: 2.5
     },
     audioConstraints: {
         betterBgAudio: {
@@ -17,6 +17,11 @@ settings = {
         normal: {
             audio: true
         },
+    },
+    theme: {
+        clrAccent: '',
+        clr: '',
+        ballsNotebg: true
     }
 }
 
@@ -36,16 +41,36 @@ settings.start = function () {
         settings.gestureSensitivity.zoom = parseFloat(this.value)
     }
 
-    document.getElementById("settings-clr").value = getComputedStyle(document.documentElement).getPropertyValue('--clr').trim()
-    document.getElementById("settings-clr").onchange = function () {
-        document.documentElement.style.setProperty('--clr', this.value)
+    document.getElementById("settings-balls-notebg").checked = settings.theme.ballsNotebg
+    document.getElementById("settings-balls-notebg").onchange()
+
+    settings.theme.clrAccent = getComputedStyle(document.documentElement).getPropertyValue('--clr-accent').trim()
+    document.getElementById("settings-clr-accent").value = settings.theme.clrAccent
+    document.getElementById("settings-clr-accent").onchange = function () {
+        document.documentElement.style.setProperty('--clr-accent', this.value)
+        settings.theme.clrAccent = this.value
     }
 
-    document.getElementById("settings-clr-secondary").value = getComputedStyle(document.documentElement).getPropertyValue('--clr-secondary').trim()
-    document.getElementById("settings-clr-secondary").onchange = function () {
-        document.documentElement.style.setProperty('--clr-secondary', this.value)
+    settings.theme.clr = getComputedStyle(document.documentElement).getPropertyValue('--clr').trim()
+    document.getElementById("settings-clr").value = settings.theme.clr
+    document.getElementById("settings-clr").onchange = function () {
+        document.documentElement.style.setProperty('--clr', this.value)
+        settings.theme.clr = this.value
     }
 
     settings.audioConstraints.default = settings.audioConstraints.betterBgAudio
+    document.getElementById("settings-betterBgAudio").checked = true
+
 }
-    
+
+settings.setValues = function () {
+    document.getElementById("settings-moveWithCtrl").value = settings.gestureSensitivity.moveWithCtrl
+    document.getElementById("settings-moveWithFinger").value = settings.gestureSensitivity.moveWithFinger
+    document.getElementById("settings-zoom").value = settings.gestureSensitivity.zoom
+    document.getElementById("settings-clr-accent").value = settings.theme.clrAccent
+    document.getElementById("settings-clr").value = settings.theme.clr
+    document.getElementById("settings-balls-notebg").checked = settings.theme.ballsNotebg
+    document.getElementById("settings-balls-notebg").onchange()
+    document.getElementById("settings-betterBgAudio").checked = settings.audioConstraints.default === settings.audioConstraints.betterBgAudio
+    document.getElementById("settings-betterBgAudio").onchange()
+}
